c1a08af0b17adb7c06126ea7a50118e820a9f522
VINAYREDDY <vinay10498d@gmail.com>
2023-02-24 17:23:42 +0530

    Program to find the first Repeating Element in a given Integer Array

--- ArrayRepeatingElements.java
+++ ArrayRepeatingElements.java
@@ -4,7 +4,7 @@ class ArrayRepeatingElements
 {
 	public static void main(String[] args) 
 	{
-		int[] arr = {10, 5, 20, 15, 25, 20, 10, 5};
+		int[] arr = {10, 12, 20, 15, 25, 20, 1, 5};
 
 		//Array Elements before task
 		System.out.println("Elements present in an Array is : ");
@@ -12,14 +12,16 @@ class ArrayRepeatingElements
 			System.out.print(arr[i]+"  ");
 		}
     
-			// Finding the first repeating element
-			for (int i = 0; i < arr.length; i++) {
-			  for (int j = i + 1; j < arr.length; j++) {
+		// Finding the first repeating element
+		System.out.println("\nFirst Repeating Element in a given Integer Array with its Index are : ");
+		End:
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[i] == arr[j]) {
-				  System.out.println("\nFirst repeating element: " + arr[i]);
-				  System.out.println("Index of first repeating element: " + i);
+				  System.out.println("Element: " + arr[i]+" Index "+i);
+				  break End;
 				}
-			  }
-			}
+			 }
+		}
 	}
 }
